93c22aef4a2bb74d61e34b35b4b573ef37ce5402
Branislav Tomic <btomic@yahoo.com>
2017-04-17 15:09:06 +0200

    Salje se JSON string na server!

--- Bojler/user/user_main.c
+++ Bojler/user/user_main.c
@@ -2,22 +2,19 @@
 	The hello world demo
 */
 
-#include <ets_sys.h>
-#include <osapi.h>
+//#include <mem.h>
+//#include <ets_sys.h>
+//#include <osapi.h>
 #include "user_interface.h"
 #include <os_type.h>
 #include <gpio.h>
 #include "driver/uart.h"
+#include "httpclient.h"
+#include "parson.h"
 
-#define DELAY 1000 /* milliseconds */
-
+#define DELAY 5000 /* milliseconds */
 LOCAL os_timer_t hello_timer;
 extern int ets_uart_printf(const char *fmt, ...);
-
-LOCAL void ICACHE_FLASH_ATTR hello_cb(void *arg)
-{
-	ets_uart_printf("Hello World!\r\n");
-}
 
 /******************************************************************************
  * FunctionName : user_rf_cal_sector_set
@@ -69,31 +66,46 @@ void ICACHE_FLASH_ATTR user_rf_pre_init(void)
 
 }
 
-void scanCB(void *arg, STATUS status) {
-	struct bss_info *bssInfo;
-	bssInfo = (struct bss_info *)arg;
-	// skip the first in the chain … it is invalid
-	bssInfo = STAILQ_NEXT(bssInfo, next);
-	while(bssInfo != NULL) {
-		ets_uart_printf("ssid: %s\n", bssInfo->ssid);
-		bssInfo = STAILQ_NEXT(bssInfo, next);
-	}
-	// Set up a timer to send the message
-	os_timer_disarm(&hello_timer);
-	os_timer_setfn(&hello_timer, (os_timer_func_t *)hello_cb, (void *)0);
-	os_timer_arm(&hello_timer, DELAY, 1);
+void ICACHE_FLASH_ATTR wifi_handle_event_cb(System_Event_t *evt)
+{
+	ets_uart_printf("event %x\n", evt->event);
+	int res;
+    switch (evt->event) {
+         case EVENT_STAMODE_GOT_IP:
+        	 ets_uart_printf("dobio ip adresu!\n");
+       		JSON_Value *val = NULL;
+       		JSON_Object *obj = NULL;
+       		val = json_value_init_object();
+       		obj = json_value_get_object(val);
+       		if(json_object_set_string(obj, "rec", "nova lozinka") != JSONSuccess)
+       			return;
+       		if(json_object_set_number(obj, "broj", 12345) != JSONSuccess)
+       			return;
+       		char * bafer = json_serialize_to_string(val);
+       		if(bafer == NULL){
+       			ets_uart_printf("bafer = NULL!\n");
+       		}
+       		else
+       		{
+       			ets_uart_printf("poslato:\n%s\n", bafer);
+       			http_post("http://btomic.000webhostapp.com/test_json_primi_remote.php", bafer, "", http_callback_example);
+//         	 http_post("http://btomic.000webhostapp.com/test_json_primi_remote.php", "{\"broj\":\"333\",\"rec\":\"bilosta\"}", "", http_callback_example);
+////        	 http_get("http://btomic.000webhostapp.com/vremena.php?sat1=148", "", http_callback_example);
+//
+       		}
+              break;
+         default:
+             break;
+ }
 }
 
+
 void ICACHE_FLASH_ATTR init_done() {
-	// Configure the UART
-//	uart_init(BIT_RATE_115200, BIT_RATE_115200);
-	// Ensure we are in station mode
-	wifi_set_opmode_current(STATION_MODE);
-	// Request a scan of the network calling "scanCB" on completion
-	wifi_station_scan(NULL, scanCB);
+	ets_uart_printf("zavrsio init!\n");
 }
 
 void ICACHE_FLASH_ATTR user_init(void)
 {
+	wifi_set_event_handler_cb(wifi_handle_event_cb);
 	system_init_done_cb(init_done);
 }
